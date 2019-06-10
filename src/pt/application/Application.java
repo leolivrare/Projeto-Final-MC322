d5157c48e18434c86780069dae61d3ee9277f8d7
SerodioJ <48109057+SerodioJ@users.noreply.github.com>
2019-06-10 15:10:40 -0300

    Update Application.java

--- src/pt/application/Application.java
+++ src/pt/application/Application.java
@@ -28,48 +28,63 @@ public class Application {
 	public static void main(String[] args) {
 		boolean aux = true;
 		Scanner sc = new Scanner(System.in);
-		IAnimationC animation = new AnimationC();
-		
-		
-		while (aux) {			
-			DataOrganizer dataOr = new DataOrganizer();
-			DataSetComponent dataset = new DataSetComponent();
-			IFileUsage fileUsage = new FileUsage();
-			IDialogue dialogue = new DialogueCreator(0);
-			Tree tree;
-			int[][] symptomFrequency;
+		IAnimationC animation = new AnimationC();		
+		DataOrganizer dataOr = new DataOrganizer();
+		DataSetComponent dataset = new DataSetComponent();
+		IFileUsage fileUsage = new FileUsage();
+		IDialogue dialogue = new DialogueCreator(0);
+		Tree tree;
+		int[][] symptomFrequency;
 			
-			IDoctor doctor = new Doctor("Doctor Variolla");
+		IDoctor doctor = new Doctor("Doctor Variolla");
 			
-			animation.setDocName("Doctor Variolla");
-			animation.setWindowName("Consultorio");
+		animation.setDocName("Doctor Variolla");
+		animation.setWindowName("Consultorio");
 			
-			//Deixa o usuario escolher o caminho da tabela ou da serializacao da arvore
-	        String path = Path.getPath(".csv", ".txt");
+		//Deixa o usuario escolher o caminho da tabela ou da serializacao da arvore
+	    String path = Path.getPath(".csv", ".txt");
 			
-			//Se ele escolher a tabela o programa ira construir a arvore, se ele escolher a arvore o jogo ira carregar ela
-			if (path.contains(".csv")) {
-			    dataset.setDataSource(path);
-			    String[][] instances = dataset.requestInstances();
+		//Se ele escolher a tabela o programa ira construir a arvore, se ele escolher a arvore o jogo ira carregar ela
+		if (path.contains(".csv")) {
+			dataset.setDataSource(path);
+			String[][] instances = dataset.requestInstances();
 			    
-			    List<String> diseases = dataOr.diseaseFilter(instances);
-			    symptomFrequency = dataOr.symptomFilter(instances, diseases);
-			    tree = dataOr.treeMaker(diseases, symptomFrequency, instances);
+		    List<String> diseases = dataOr.diseaseFilter(instances);
+			symptomFrequency = dataOr.symptomFilter(instances, diseases);
+			tree = dataOr.treeMaker(diseases, symptomFrequency, instances);
 			    
-			    doctor.connect(dataset);
-			    doctor.setTree(tree);
-			} else {
-				String folderName =  Path.getFolderName();
-				String pathCSV = fileUsage.getPathCSV(folderName);
-				System.out.println("Path: "+pathCSV);
-				dataset.setDataSource(pathCSV);
-			    symptomFrequency = fileUsage.getFrequency(folderName);
+			doctor.connect(dataset);
+			doctor.setTree(tree);
+		} else {
+			String folderName =  Path.getFolderName();
+			String pathCSV = fileUsage.getPathCSV(folderName);
+			System.out.println("Path: "+pathCSV);
+			dataset.setDataSource(pathCSV);
+			symptomFrequency = fileUsage.getFrequency(folderName);
 				
-				tree = fileUsage.getTree(folderName);
-				doctor.connect(dataset);
-				doctor.setTree(tree);
+			tree = fileUsage.getTree(folderName);
+			doctor.connect(dataset);
+			doctor.setTree(tree);
+		}
+		while(aux) {
+			System.out.println("Deseja salvar os dados (S/N)?");
+			Character c = sc.next().charAt(0);
+			if (c.equals('S')) {
+				fileUsage.save(tree.getDiseases(), symptomFrequency, tree, path);
+				System.out.println("Dados salvos com sucesso!");
+				aux = false;
 			}
-			
+			else if (c.equals('N')) {
+				aux = false;
+			}
+			else {
+				System.out.println("Opcao invalida!");
+			}
+		}
+		
+		aux = true;
+		
+		while (aux) {	
 			System.out.print("Digite o nome do Paciente (somente o primeiro nome): ");
 			String patientName = sc.next();
 			System.out.println();
@@ -97,25 +112,6 @@ public class Application {
 			
 			animation.story(falas, personagem);
 			boolean aux2 = true;
-			
-			
-			while(aux2) {
-				System.out.println("Deseja salvar os dados (S/N)?");
-				Character c = sc.next().charAt(0);
-				if (c.equals('S')) {
-					fileUsage.save(tree.getDiseases(), symptomFrequency, tree, path);
-					System.out.println("Dados salvos com sucesso!");
-					aux2 = false;
-				}
-				else if (c.equals('N')) {
-					aux2 = false;
-				}
-				else {
-					System.out.println("Opcao invalida!");
-				}
-			}
-			
-			aux2 = true;
 			
 			while(aux2) {
 				System.out.println("Deseja analisar outro paciente (S/N)?");
